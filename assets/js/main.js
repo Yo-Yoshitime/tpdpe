2f9e6fb3ba92c5c603cafcf7d10b02afedc897ef
Yoshitime <Asdrubalini.inc@gmail.com>
2025-01-09 04:20:34 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+//Table 1
   function statusList(evt, Status) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -12,8 +12,9 @@
   document.getElementById(Status).style.display = "block";
   evt.currentTarget.className += " active";
 }
+  document.getElementById("defaultOpen").click();
 
-
+//Table 2
 function statusNoList(evt, StatusNo) {
   var i, tabcontent2, tablinks2;
   tabcontent = document.getElementsByClassName("tabcontent2");
@@ -26,9 +27,10 @@ function statusNoList(evt, StatusNo) {
   }
   document.getElementById(StatusNo).style.display = "block";
   evt.currentTarget.className += " active";
-}
+} 
+  document.getElementById("defaultOpen2").click();
 
-
+//Table 3
 function statusTempList(evt, statusTemp) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent3");
@@ -41,4 +43,32 @@ function statusTempList(evt, statusTemp) {
   }
   document.getElementById(statusTemp).style.display = "block";
   evt.currentTarget.className += " active";
+}
+  document.getElementById("defaultOpen3").click();
+
+
+// Scroll for 1, 2 and 3
+const scrollableArea = document.getElementById("center");
+const scrollableArea2 = document.getElementById("center2");
+const scrollableArea3 = document.getElementById("center3");
+
+function ScrollLeft() {
+	scrollableArea.scrollBy(-400, 0)
+}
+function ScrollRight() {
+	scrollableArea.scrollBy(400, 0)
+}
+
+function ScrollLeft2() {
+	scrollableArea2.scrollBy(-400, 0)
+}
+function ScrollRight2() {
+	scrollableArea2.scrollBy(400, 0)
+}
+
+function ScrollLeft3() {
+	scrollableArea3.scrollBy(-400, 0)
+}
+function ScrollRight3() {
+	scrollableArea3.scrollBy(400, 0)
 }
